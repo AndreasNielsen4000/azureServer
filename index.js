2498182c5df28a0fcacc4bb48664e3314b07545f
Andreas Nielsen <andreasnielsen4000@gmail.com>
2024-04-14 19:36:49 +0200

    Switch mode back again

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const express = require("express");
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const express = require('express')
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send("Hello, Server!");
+// Use Node.js body parsing middleware 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// GET method route
+app.get('/', (request, response) => {
+    response.send({
+        message: 'Node.js and Express REST API'
+    });
 });
 
+//Get users
+app.get('/users', (request, response) => {
+    response.send(users);
+});
+
+// Start the server 
 const server = app.listen(port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
-	console.log(`Server listening on port ${server.address().port}`);
+  if (error) return console.log(`Error: ${error}`);
+  console.log(`Server listening on port ${server.address().port}`);
 });
+
+
+
+const users = [
+    {
+      id: 1,
+      name: "Richard Hendricks",
+      email: "richard@piedpiper.com",
+    },
+    {
+      id: 2,
+      name: "Bertram Gilfoyle",
+      email: "gilfoyle@piedpiper.com",
+    },
+];
