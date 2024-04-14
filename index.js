c4ce58c1c4621c3b439e301891bd8d6bc782e478
Andreas Nielsen <andreasnielsen4000@gmail.com>
2024-04-14 19:10:45 +0200

    test1

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-/* import bodyParser from "body-parser";
-import express from "express";
-import routes from "./routes/routes.js";
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Use Node.js body parsing middleware 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-routes(app);
-// Start the server 
-const server = app.listen(port, (error) => {
-  if (error) return console.log(`Error: ${error}`);
-  console.log(`Server listening on port ${server.address().port}`);
-}); */
-
-import express from "express";
+const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
